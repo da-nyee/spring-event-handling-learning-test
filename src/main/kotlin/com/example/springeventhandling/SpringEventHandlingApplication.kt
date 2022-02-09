bdb31b7b831d77e30bdbbc32e4940d744feeb09c
Daeun Lee <leede0418@likelion.org>
2022-02-10 00:50:37 +0900

    chore: 프로젝트 기본 세팅

---
+++ src/main/kotlin/com/example/springeventhandling/SpringEventHandlingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springeventhandling
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringEventHandlingApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringEventHandlingApplication>(*args)
+}
