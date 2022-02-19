d5755b023fc29e69b2cbe98a385b0ce8e5db04b9
da-nyee <leede0418@gmail.com>
2022-02-19 23:46:49 +0900

    chore: 주석 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,11 @@ repositories {
 }
 
 dependencies {
+	// Spring
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// Kotlin
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
